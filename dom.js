3eaa02dc075abbf1137ed6e02b1cba8958fdad16
Shukurah <bakareshukurah@outlook.com>
2021-03-18 19:50:59 +0100

    commit

--- dom.js
+++ dom.js
@@ -9,96 +9,98 @@ console.log(document.all[6]);
 
 //DOM selectors:There are few methods by which we can query the DOM
 // GETELEMENTBYID//
-//console.log(document.getElementById("show"));
-//let showCase = document.getElementById("show");
-//console.log(showCase);//mine appears to show null in the broswer console.
-let work = document.getElementsByTagName("h2");
-console.log(work)
+console.log(document.getElementById("show"));
+let showCase = document.getElementById("show");
+showCase.innerText = "Learning can be tiring too"
+
 
 //There are two methods to changing text which are: textContent and innerText.
 //The textContent method does not pay attention to the styling of an element while the innerText method does.
-// work.textContent = "hello";
-// console.log(work.textContent);
+//work.textContent = "hello";
+//console.log(work.textContent);
 
 // Styles can also be changed using the dot notation .style and .the style to be changed
-// work.style.borderBottom = 'solid 3px black';
+showCase.style.borderBottom ='solid 3px black';
 
 // GETELEMENTSBYCLASSNAME
-//let items = document.getElementsByClassName('list-group-item');
-//console.log(items)
-//items[1].textContent = 'welcome';
+let items = document.getElementsByClassName('list-group-item');
+// console.log(items)
+items[0].innerText = 'Please';
+items[1].innerText = 'Excuse Me';
+items[2].innerText = 'Sorry';
+items[3].innerText = 'Thank You';
+items[4].innerText = 'Pardon Me';
 
 
 //QUERYSELECTOR//
 //Grabs just the first item of whatever you are trying to get
-//let headerText = document.querySelector('#show');
-//headerText.style.borderBottom = "2px solid black";
- //ErrorMessage:cannot read property style of null
+let headerText = document.querySelector('#show');
+headerText.style.color = "green";
+ 
 
 //QUERYSELECTORALL//
-//Selects all 
+console.log (document.querySelectorAll('li'))
 
 
 //TRAVERSING THE DOM//
-//let itemList = document.querySelector('#items');
+let itemList = document.querySelectorAll('li')[2];
 
 //ParentNode
-///console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = "black";
-// console.log(itemList.parentNode.parentNode)//This gives us the parent to the parent of itemList
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "black";
+console.log(itemList.parentNode.parentNode)
+itemList.parentNode.parentNode.style.backgroundColor = "yellow";
 
 // ParentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = "black";
-// console.log(itemList.parentElement.parentElement)//This gives us the parent to the parent of itemList
-//ParentNode and ParentElement can be used interchangeably
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = "black";
 
 //ChildNodes and Children
+const form = document.querySelector('form');
 //console.log(itemList.childNodes);
-//console.log(itemlist.children);
+console.log(form.children);
 
 // firstChild
-//console.log(itemList.firstChild);
+console.log(form.firstChild);
 //firstElementChild
-//console.log(itemList.firstElementChild);
+console.log(form.firstElementChild);
 
 // lastChild
-//console.log(itemList.lastChild);
+console.log(form.lastChild);
 //lastElementChild
-//console.log(itemList.lastElementChild);
+console.log(form.lastElementChild);
 
 //nextSibling
-//console.log(itemList.nextSibling);
+console.log(form.nextSibling);
 //nextSibling
-//console.log(itemList.nextSibling);
+console.log(form.nextElementSibling);
 
 //previousSibling
-//console.log(itemList.previousSibling);
-//previousSibling
-//console.log(itemList.previousSibling);
+console.log(form.previousSibling);
+
 
 //previousElementSibling
-//console.log(itemList.previousElementSibling);
-//previousElementSibling
-//console.log(itemList.previousElementSibling);
+console.log(form.previousElementSibling);
+//nextElementSibling
+console.log(form.nextElementSibling);
 
 //createElement
 
 //Create a div
-//let createDiv = document.createElement('div');
+let createDiv = document.createElement('div');
 //adiing a class name to the Div
-//createDiv.className = "Welcome";
+createDiv.className = "Welcome";
 //Add id
-//createDiv.id = "You are Welcome";
+createDiv.id = "You are Welcome";
 //Add Attribute
-//createDiv.setAttribute('title','Welcome Message');
+createDiv.setAttribute('title','Welcome Message');
 //Create text node
-//let divText =  document.createTextNode('Congratulations');
+let divText =  document.createTextNode('Congratulations');
 //Add text to div
-//createDiv.appendChild(divText);
+createDiv.appendChild(divText);
 
 //Insert New div into DOM
-//let insert = document.querySelector('header .navbar');
+let insert = document.querySelector('header .navbar');
 //let h2 = document.querySelector('header .navbar h1');
 
 //console.log(createDiv);
