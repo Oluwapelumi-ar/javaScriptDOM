54127b026dd2ea76076f2e59d55002856eed72f3
Shukurah <bakareshukurah@outlook.com>
2021-03-19 09:30:21 +0100

    edited

--- magic-words.js
+++ magic-words.js
@@ -8,12 +8,20 @@ console.log(document.all[6]);
 
 
 // GETELEMENTBYID//
-console.log(document.getElementById("show"));
-let showCase = document.getElementById("show");
-showCase.innerText = "There are five magic words"
+console.log(document.getElementById("fiveMagicWords"));
+let showCase = document.getElementById("fiveMagicWords");
+showCase.innerText = "There are five magic words";
+showCase.previousElementSibling.style.color = 'black';
+
 
 // Implementing styles from the DOM
-showCase.style.borderBottom ='solid 3px black';
+showCase.style.borderBottom ='solid 2px black';
+let jumbotron = document.querySelector('.jumbotron');
+console.log(jumbotron);
+jumbotron.style.backgroundColor = 'green'
+console.log(jumbotron.lastElementChild);  
+jumbotron.lastElementChild.style.backgroundColor = 'black';
+
 
 // GETELEMENTSBYCLASSNAME
 let items = document.getElementsByClassName('list-group-item');
@@ -24,7 +32,7 @@ items[3].innerText = 'Thank You';
 items[4].innerText = 'Pardon Me';
 
 //QUERYSELECTOR//
-let headerText = document.querySelector('#show');
+let headerText = document.querySelector('#fiveMagicWords');
 headerText.style.color = "black";
 
 //QUERYSELECTORALL//
@@ -44,50 +52,12 @@ itemList.parentElement.style.backgroundColor = 'black';
 //Children
 const form = document.querySelector('form');
 console.log(form.children);
-
-// firstChild
-console.log(form.firstChild);
-//firstElementChild
-console.log(form.firstElementChild);
+form.style.backgroundColor = 'white';
 
 // lastChild
 console.log(form.lastChild);
 //lastElementChild
 console.log(form.lastElementChild);
-form.lastElementChild.style.backgroundColor = "black";
-//nextSibling
-console.log(form.nextSibling);
-//nextSibling
-console.log(form.nextElementSibling);
-
-//previousSibling
-console.log(form.previousSibling);
+form.lastElementChild.style.backgroundColor = 'green';
 
 
-//previousElementSibling
-console.log(form.previousElementSibling);
-//nextElementSibling
-console.log(form.nextElementSibling);
-
-//createElement
-
-//Create a div
-let createDiv = document.createElement('div');
-//adiing a class name to the Div
-createDiv.className = "Welcome";
-//Add id
-createDiv.id = "You are Welcome";
-//Add Attribute
-createDiv.setAttribute('title','Welcome Message');
-//Create text node
-let divText =  document.createTextNode('Congratulations');
-//Add text to div
-createDiv.appendChild(divText);
-
-//Insert New div into DOM
-let insert = document.querySelector('header .navbar');
-//let h2 = document.querySelector('header .navbar h1');
-
-//console.log(createDiv);
-
-//insert.insertBefore(createDiv, h2); //....Error Message:caanot read property 'insertBefore' of null
